22e2d31443e3d38539b9d1a4c17bc7e6c5794368
Ahmed Hawas <ahmedhawas7@gmail.com>
2024-08-18 17:09:47 -0400

    hardcode url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://polar-reef-73313-c2c5d8b9a403.herokuapp.com/api/v1",
 });
 
 // Add a request interceptor to include the auth token in all requests
